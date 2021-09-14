7d2598bcf3a3edfcc13bdabe8c34eeb93012dc4b
TsvetanG <tsvetan.georgiev@gmail.com>
2021-09-13 21:14:46 -0400

    Update install-bins.sh
    Add script to download fabric CA binaries

--- install-bins.sh
+++ install-bins.sh
@@ -8,3 +8,4 @@ CA_BINARY_FILE=hyperledger-fabric-ca-${ARCH}-${CA_VERSION}.tar.gz
 
 curl -L --retry 5 --retry-delay 3 "https://github.com/hyperledger/fabric/releases/download/v${VERSION}/${BINARY_FILE}" | tar xz || rc=$?
 
+curl -L --retry 5 --retry-delay 3 "https://github.com/hyperledger/fabric-ca/releases/download/v${CA_VERSION}/${CA_BINARY_FILE}" | tar xz || rc=$?
