484dc6580a76794a2c0cbe0f83616188ec8a8153
TsvetanG <tsvetan@senofi.ca>
2021-09-13 20:51:19 -0400

    Add script to install HLF binaries from HLF repos

---
+++ install-bins.sh
@@ -0,0 +1,10 @@
+VERSION=2.3.3
+CA_VERSION=1.5.2
+ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')")
+MARCH=$(uname -m)
+BINARY_FILE=hyperledger-fabric-${ARCH}-${VERSION}.tar.gz
+CA_BINARY_FILE=hyperledger-fabric-ca-${ARCH}-${CA_VERSION}.tar.gz
+
+
+curl -L --retry 5 --retry-delay 3 "https://github.com/hyperledger/fabric/releases/download/v${VERSION}/${BINARY_FILE}" | tar xz || rc=$?
+
