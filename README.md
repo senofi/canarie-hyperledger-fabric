7b304f4bf43ac9e3543aa9b44db886e54aa4712f
Yanko Zhelyazkov <yanko@senofi.ca>
2021-09-17 13:29:23 -0700

    Update the script with a few fixes

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 Sample Solution of a Hyperledger Fabric Network, chaincode and web applications.
 
 CANARIE DAIR Sample Solution
+
+Prerequisites for this solution to work are Docker, Docker Compose and Git.
+
 Preliminary steps to run on a Linux machine:
 - Run ./scripts/install-bins.sh
 - Go to folder "fabric-network"
@@ -25,8 +28,14 @@ Assume we have a javascript chaincode in folder ../../fabric-samples/asset-trans
 
 ## Locally
 
+The sample solution could be installed locally using a [Vagrant|https://www.vagrantup.com/] virtual machine (VM). The default provider that Vagrant relies on is [VirtualBox|https://www.virtualbox.org/]. Make sure that both of them are installed locally before proceeding.
+
+To get the VM up and running simply run `vagrant up` in the root folder.
+
 ## Cloud Provider
 
+We support Canarie DAIR Cloud and you can find more information on our BoosterPack and others [here|https://www.canarie.ca/cloud/boosterpacks/].
+The only limitation for any other cloud provider is that the operating system is Ubuntu 20.04 or above.
 # License
 
 All code and configuration in this repository (code files contained in this directory and all subdirectories) is licensed under the [Apache 2.0 License](http://www.apache.org/licenses/LICENSE-2.0).
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-###
-# Startup script to set up GIT, clone the repository and start the process
-###
+#################################################################################
+# Startup script to set up required prerequisites, clone the repository 
+# and start the process for setting Hyperledger Fabric and the Sample Solution.
+#################################################################################
 
-apt-get update
-apt-get install git
+apt-get update && \
+    apt-get -y install git && \
+    apt-get -y install docker.io && \
+    apt-get -y install docker-compose
 
-git clone -b ${1:-develop} https://github.com/senofi/canarie-hyperledger-fabric.git && \
-    cd canarie-hyperledger-fabric && \
-    ./scripts/install-bins.sh && \
-    cd fabric-network && \
+HLF_FOLDER=/opt/canarie/hyperledger-fabric
+BRANCH=${1:-develop}
+mkdir -p $HLF_FOLDER
+
+echo "Cloning Git repository with branch: '$BRANCH'"
+git clone -b $BRANCH https://github.com/senofi/canarie-hyperledger-fabric.git $HLF_FOLDER && \
+    cd $HLF_FOLDER && \
+    $HLF_FOLDER/scripts/install-bins.sh && \
+    cd $HLF_FOLDER/fabric-network && \
     ./network.sh down && \
     ./network.sh up -ca && \
     ./network.sh createChannel -c trial && \
     ./network.sh deployCC -ccn cctest -ccp ../clinical-trials-chaincode/ -ccl java -c trial && \
-    docker-compose -f docker/docker-compose-web-apps.yaml up -d
+    docker-compose -f docker/docker-compose-web-apps.yaml up -d && \
+    echo "Installation Process Complete"
